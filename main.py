6ccc644ae1b5ec1006ee26b92a838e6492be6705
Mr-xy9 <alhusinalyami@gmail.com>
2025-09-09 14:25:19 +0300

    add task

---
+++ main.py
@@ -0,0 +1,7 @@
+from mypackage import calc_utils, weather_utils
+
+print(calc_utils.square(5))
+print(calc_utils.cube(3))
+weather_utils.today_weather("Riyadh")
+
+weather_utils.forecast("Riyadh")
---
+++ mypackage/weather_utils.py
@@ -0,0 +1,6 @@
+def today_weather(r):
+    print(f"The weather in {r} is sunny with mild temperature.")
+    
+
+def forecast(r):
+    print(f"The forecast for {r} is rainy for the next two days.")
